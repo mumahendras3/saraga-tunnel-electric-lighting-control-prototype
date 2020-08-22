bfda839b0415f38a89f5cbaf25c27786d014b46d
Muhammad Mahendra Subrata <mumahendras3@gmail.com>
2020-08-22 22:53:39 +0700

    Implemented file uploading mechanisms and image extracting from pdfs

--- public/script/index.js
+++ public/script/index.js
@@ -3,7 +3,7 @@ const serverAddress = 'localhost'; // MQTT server address
 const serverPort = 3000; // MQTT server port (using websocket)
 const totalData = 3; // the total number of sensor readings to collect per cycle
 const waitTime = 500; // time (in ms) to wait before rechecking if all sensors have sent their readings
-var dataCounter = 0; // counter to keep track of how many sensor readings have been collected per cycle
+let dataCounter = 0; // counter to keep track of how many sensor readings have been collected per cycle
 
 // MQTT Setup
 const client = mqtt.connect('ws://' + serverAddress + ':' + serverPort);
@@ -12,9 +12,11 @@ const client = mqtt.connect('ws://' + serverAddress + ':' + serverPort);
 client.subscribe("status/#");
 
 // Get elements
-var sensor = {};
-var lampu = {};
-var grafik = {};
+let uploadForm = document.querySelector('form.section');
+let uploadInput = document.querySelector('[name=upload]');
+let sensor = {};
+let lampu = {};
+let grafik = {};
 document.querySelectorAll(".sensor").forEach(function(element){
     sensor[element.id] = element;
 });
@@ -45,8 +47,8 @@ function statusSensor(id, value) {
 };
 function statusWaktu(waktu) {
     // Convert unix timestamp to hour:minute:seconds
-    var time = new Date(waktu);
-    var timeStr = ('0' + time.getHours()).slice(-2) + ':';
+    let time = new Date(waktu);
+    let timeStr = ('0' + time.getHours()).slice(-2) + ':';
     timeStr += ('0' + time.getMinutes()).slice(-2) + ':';
     timeStr += ('0' + time.getSeconds()).slice(-2);
     // Loop until all sensors have sent their readings
@@ -69,19 +71,81 @@ function setBatas(event) {
     event.preventDefault();
     client.publish("set/" + this.className + "/" + this.id, this.children[1].value);
 };
+function upload(form) {
+    // Create a new XMLHttpRequest (XHR) DOM object instance
+    const XHR = new XMLHttpRequest();
+    // Bind the FormData object and the form element
+    const FD = new FormData(form);
+    // Define what happens on successful data submission
+    XHR.addEventListener("load", event => {
+      alert(event.target.responseText);
+    });
+    // Define what happens in case of error
+    XHR.addEventListener("error", event => {
+      alert('Oops! Something went wrong.');
+    });
+    // Set up our request
+    XHR.open("POST", "/upload");
+    // The data sent is what the user provided in the form
+    XHR.send(FD);
+}
+function addImgNode(id) {
+    fetch('/get-images')
+    .then(res => res.json())
+    .then(res => {
+        let parentNode = document.getElementById(id);
+        let count = 0; // To prevent double titles
+        res.forEach(file => {
+            if (file == 'place_holder.png') return;
+            // Save the area name first
+            let areaName = file.slice(0, -6);
+            // Create the image node
+            let imgElement = document.createElement('img');
+            // Set the img source
+            imgElement.setAttribute('src', 'images/' + file);
+            // Setting the appropriate alt text
+            let altStr = 'Simulasi distribusi iluminansi di area ';
+            altStr += areaName;
+            imgElement.setAttribute('alt', altStr);
+            // Set the class to 'image'
+            imgElement.setAttribute('class', 'image');
+            // Attach all new nodes to the parent node
+            // Create only 1 title for each image pair (illuminance dist. and its legend)
+            if (count < 1) {
+                let title = document.createElement('h2');
+                title.innerHTML = areaName;
+                parentNode.appendChild(title)
+                count++;
+            }
+            else {
+                count--;
+            }
+            parentNode.appendChild(imgElement);
+        });
+    })
+    .catch(err => console.error(err));
+}
 
 // Attaching functions to some events
 document.querySelectorAll("form.batas").forEach(function(element){
     element.addEventListener("submit", setBatas);
 });
+uploadForm.addEventListener('submit', event => {
+    event.preventDefault();
+    upload(uploadForm);
+    uploadInput.value = "";
+});
 
 // MQTT upon receive
 client.on('message', function(topic, payload){
-    var split = topic.split("/");
-    var payloadStr = payload.toString();
+    let split = topic.split("/");
+    let payloadStr = payload.toString();
     switch (split[0] + "/" + split[1]) {
         case "status/lampu": statusLampu(split[2], payloadStr); break;
         case "status/sensor": statusSensor(split[2], parseFloat(payloadStr)); break;
         case "status/waktu": statusWaktu(payloadStr); break;
     }
 });
+
+// Show simulation results of all available setpoint values (taken from the uploaded pdf files)
+addImgNode('section-simulation')
--- server.js
+++ server.js
@@ -11,11 +11,14 @@ const mqttPort = 1883; // MQTT server port
 const httpPort = 3000; // HTTP web server port
 const totalData = 3; // The total number of sensor readings to collect per cycle
 const waitTime = 1000; // Time (in ms) to wait before rechecking if all sensors have sent their readings
-const illuminanceRegex = /Ē\n([0-9]+)\slx/m; // The regex to parse the desired illuminance value from the given pdf files
-const setpointsDir = "setpoints" // the directory that contains all the pdf files that will be parsed
-var setpoints = []; // An array to hold all the setpoints data at runtime
-var data = []; // Temporary data holder
-var db; // Variable to store the database object after successfull connection to MongoDB
+const illuminanceRegex = /Ē\n([0-9.]+)\slx/m; // The regex to parse the desired illuminance value from the given pdf files
+const setpointsDir = 'setpoints'; // the directory that contains all setpoint json files
+const imagesDir = 'public/images'; // Public images directory
+const binDir = 'bin'; // external executable files directory
+let setpoints = []; // An array to hold all the setpoints data at runtime
+let sent = {}; // An object that contains the last time setpoints have been sent for each area/location
+let data = []; // Temporary data holder
+let db; // Variable to store the database object after successfull connection to MongoDB
 
 // Function definitions
 function simpanData(waktu) {
@@ -40,7 +43,7 @@ function readSetpoints(setpointsDir, setpointsArray, regex) {
     fs.readdir(setpointsDir, (err, files) => {
         if (err) throw err;
         if (files.length == 0) {
-            console.log("No files found in " + setpointsDir + "/, skipping read setpoints...");
+            console.log('No files found in ' + setpointsDir + ', skipping read setpoints...');
             return;
         }
         // Reset the given setpointsArray first
@@ -48,38 +51,76 @@ function readSetpoints(setpointsDir, setpointsArray, regex) {
         // not a local copy of it (i.e. to do a pass-by-reference for the setpointsArray parameter).
         setpointsArray.splice(0, setpointsArray.length);
         files.forEach(file => {
-            if (path.posix.extname(file) != ".pdf") return;
+            if (path.posix.extname(file) != '.pdf') return;
             let setpoint = {};
-            fs.readFile(setpointsDir + "/" + file, (err, dataBuffer) => {
+            fs.readFile(path.posix.join(setpointsDir, file), (err, dataBuffer) => {
                 if (err) throw err;
                 pdf(dataBuffer).then(data => {
                     // let match = regex.exec(data.text);
                     // console.log(match[1]);
                     setpoint['illuminance'] = regex.exec(data.text)[1];
+                }, err => {
+                    throw err;
                 });
             });
-            let split = file.replace(".pdf", "").toLowerCase().split("-");
+            let split = file.replace('.pdf', '').toLowerCase().split('-');
             setpoint['time'] = split[0];
-            setpoint['area'] = split[1].replace(/\s|_+/g, "-");
+            setpoint['area'] = split[1].replace(/\s|_+/g, '-');
             setpointsArray.push(setpoint);
         });
     });
 }
-function sendSetpoints(setpointsArray) {
+function sendSetpoints(setpointsArray, lastSendTime) {
     if (setpointsArray.length == 0) {
-        console.log("The given setpoints array is empty, skipping send setpoints...");
+        console.log('The given setpoints array is empty, skipping send setpoints...');
         return;
     }
     let now = new Date();
-    let timeStr = ("0" + now.getHours()).slice(-2) + "." + ("0" + now.getMinutes()).slice(-2);
+    let timeStr = ('0' + now.getHours()).slice(-2) + '.' + ('0' + now.getMinutes()).slice(-2);
     setpointsArray.forEach(setpoint => {
-        if (setpoint.time == timeStr) {
+        if (setpoint.time == timeStr && setpoint.time != lastSendTime[setpoint.area]) {
             aedes.publish({
-                topic: "set/setpoint/" + setpoint.area,
+                topic: 'set/setpoint/' + setpoint.area,
                 payload: setpoint.illuminance,
                 retain: true
             });
+            lastSendTime[setpoint.area] = setpoint.time;
         }
+    });
+}
+function extractImages(pdfDir, imgDir) {
+    fs.readdir(pdfDir, (err, files) => {
+        files.forEach(file => {
+            let binPath = path.posix.join(binDir, 'pdfimages.exe');
+            let pdfPath = path.posix.join(pdfDir, file);
+            let split = file.replace('.pdf', '').split('-');
+            let imgPath = path.posix.join(imgDir, split[1].replace(/_+/g, ' '));
+            const child = execFile(binPath, ['-j', '-l', '1', pdfPath, imgPath], (error, stdout, stderr) => {
+                if (error) throw error;
+                // Rename the image files so the order is right
+                // <file_name>-1.jpg for the illuminance distribution image
+                // <file_name>-2.jpg for the illuminance distribution legend image
+                fs.rename(imgPath + '-0004.jpg', imgPath + '-1.jpg', err => {
+                    if (err) throw err;
+                });
+                fs.rename(imgPath + '-0002.jpg', imgPath + '-2.jpg', err => {
+                    if (err) throw err;
+                });
+                // Clean leftover images
+                fs.unlink(imgPath + '-0000.jpg', err => {
+                    if (err) throw err;
+                });
+                fs.unlink(imgPath + '-0001.pgm', err => {
+                    if (err) throw err;
+                });
+                fs.unlink(imgPath + '-0003.pgm', err => {
+                    if (err) throw err;
+                });
+                fs.unlink(imgPath + '-0005.pgm', err => {
+                    if (err) throw err;
+                });
+            });
+        });
     });
 }
 
@@ -88,6 +129,9 @@ const fs = require('fs');
 const path = require('path');
 const pdf = require('pdf-parse');
 readSetpoints(setpointsDir, setpoints, illuminanceRegex);
+
+// Extract -map and -legend jpg images from setpoint pdfs
+extractImages(setpointsDir, imagesDir);
 
 // Initialize an MQTT server
 const aedes = require('aedes')({id: 'mqtt-server'});
@@ -99,34 +143,82 @@ mqttServer.listen(mqttPort, function(){
 // Initialize an HTTP server with express and websocket
 const express = require('express');
 const ws = require('websocket-stream');
+const fileUpload = require('express-fileupload'); // A simple express middleware for uploading files
+const { execFile } = require('child_process'); // Needed to execute pdfimages.exe (to extract images from pdf)
 const app = express();
+app.use(fileUpload({useTempFiles: true}));
 app.use(express.static('public'));
+// Add /upload POST handler (for uploading files)
+app.post('/upload', async (req, res) => {
+    // Check if there aren't any files selected
+    if (!req.files || Object.keys(req.files).length === 0) {
+        return res.status(400).send('No files selected!');
+    }
+    // For multiple files
+    if (Array.isArray(req.files.upload)) {
+        try {
+            for (let i = 0; i < req.files.upload.length; i++) {
+                await req.files.upload[i].mv(path.posix.join(setpointsDir, req.files.upload[i].name));
+            }
+            res.send('Files uploaded successfully!');
+            // Reread all setpoint values
+            readSetpoints(setpointsDir, setpoints, illuminanceRegex);
+            // Extract all illuminance distribution images from the setpoint pdfs
+            extractImages(setpointsDir, imagesDir);
+        } catch (error) {
+            res.status(500).send('Oops! Something went wrong.\n\n' + error);
+        }
+    }
+    // For single file
+    else {
+        req.files.upload.mv(path.posix.join(setpointsDir, req.files.upload.name), error => {
+            if (error) {
+                res.status(500).send('Oops! Something went wrong.\n\n' + error);
+            }    
+            else {
+                res.send('File uploaded successfully!');
+                // Reread all setpoint values
+                readSetpoints(setpointsDir, setpoints, illuminanceRegex);
+                // Extract all illuminance distribution images from the setpoint pdfs
+                extractImages(setpointsDir, imagesDir);
+            }    
+        });
+    }
+});
+// Add /get-images GET handler (for getting the list of all simulation images)
+app.get('/get-images', (req, res) => {
+    fs.readdir(imagesDir, (err, files) => {
+        if (err) return res.status(500).send('Oops! Something went wrong.\n\n' + err);
+        let fileList = JSON.stringify(files);
+        res.send(fileList);
+    });
+});
 const httpServer = require('http').createServer(app);
 ws.createServer({ server: httpServer }, aedes.handle);
 httpServer.listen(httpPort, function(){
     console.log('websocket server listening on port', httpPort);
 });
 
-// Initialize MongoDB client
-const mongoClient = new require('mongodb').MongoClient(dbURL, {
-    useUnifiedTopology: true,
-    poolSize: 20,
-});
-// Create a new connection to MongoDB server
-if (!mongoClient.isConnected()) {
-    mongoClient.connect((err, client) => {
-        if (err) throw err;
-        console.log("Connected correctly to", dbURL);
-        db = client.db(dbName);
-    });
-}
+// // Initialize MongoDB client
+// const mongoClient = new require('mongodb').MongoClient(dbURL, {
+//     useUnifiedTopology: true,
+//     poolSize: 20,
+// });
+// // Create a new connection to MongoDB server
+// if (!mongoClient.isConnected()) {
+//     mongoClient.connect((err, client) => {
+//         if (err) throw err;
+//         console.log('Connected correctly to', dbURL);
+//         db = client.db(dbName);
+//     });
+// }
 
 // Fired when a message is published
 aedes.on('publish', (packet, client) => {
     // for debugging
     console.log(packet.topic, packet.payload.toString());
-    var split = packet.topic.split('/');
-    var payloadStr = packet.payload.toString();
+    let split = packet.topic.split('/');
+    let payloadStr = packet.payload.toString();
     switch (split[0] + '/' + split[1]) {
         case 'status/sensor':
             data.push({tempat: split[2], iluminansi: parseFloat(payloadStr)});
@@ -138,4 +230,4 @@ aedes.on('publish', (packet, client) => {
 });
 
 // Run setInterval() on sendSetpoints() with 1 second delay
-const timeoutSetpoints = setInterval(sendSetpoints, 1000, setpoints);
+const timeoutSetpoints = setInterval(sendSetpoints, 1000, setpoints, sent);
